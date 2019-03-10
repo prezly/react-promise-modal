d3bc4c6fd1e5a5bd6673739c4d666a7fb7e75f71
Ivan Voskoboinyk <ivan@prezly.com>
2019-03-10 23:55:16 +0200

    Add actual implementation

--- src/index.js
+++ src/index.js
@@ -1,3 +1,43 @@
-export default function reactModal() {
+import { render, unmountComponentAtNode } from 'react-dom';
 
+const DEFAULT_DESTRUCTION_DELAY = 300;
+const DEFAULT_OPTIONS = {
+    destructionDelay: DEFAULT_DESTRUCTION_DELAY,
+};
+
+const noop = () => {};
+
+export default async function reactModal(renderModal, options = {}) {
+    const { destructionDelay } = { ...DEFAULT_OPTIONS, ...options };
+    const container = document.createElement('div');
+    document.body.appendChild(container);
+
+    function displayModal(props) {
+        render(renderModal({ ...props, show: true }), container);
+    }
+
+    function hideModal(props, callback) {
+        render(renderModal({ ...props, show: false }), container, callback);
+    }
+
+    function destroyModal() {
+        unmountComponentAtNode(container);
+        document.body.removeChild(container);
+    }
+
+    const confirmation = new Promise((resolve) => {
+        const onConfirm = () => resolve(true);
+        const onCancel = () => resolve(false);
+        displayModal({ onConfirm, onCancel });
+    });
+
+    try {
+        return await confirmation;
+    } finally {
+        const onConfirm = noop;
+        const onCancel = noop;
+        hideModal({ onConfirm, onCancel }, () => {
+            setTimeout(destroyModal, destructionDelay);
+        });
+    }
 }
